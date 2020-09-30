9d2d2475f6e2e30eb0f3687bf9921b582419e36d
QJNel <quintinnel9@gmail.com>
2020-09-30 15:25:14 +0200

    Added CV button and improved layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var require: any;
+const FileSaver = require('file-saver');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'CV';
+export class AppComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  downloadPdf(pdfUrl: string, pdfName: string ) {
+    FileSaver.saveAs(pdfUrl, pdfName);
+  }
+
 }
